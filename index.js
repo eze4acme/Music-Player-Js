24c6b76ef1f0757652c057d5ba71aa28d58310ec
Eze Onyebuchi <eze4acme@gmail.com>
2023-08-05 17:17:12 +0100

    update file
    Js code update

---
+++ index.js
@@ -0,0 +1,28 @@
+const song = document.getElementById('song')
+const progress = document.getElementById('progress')
+const ctrlIcon = document.getElementById('ctrlIcon')
+const playPause = document.getElementById('playPause')
+
+// console.log(song);
+song.onloadedmetadata = function () {
+    progress.max = song.duration
+    progress.value = song.currentTime
+}
+
+playPause.addEventListener('click', function(){
+    if (ctrlIcon.classList.contains('fa-pause')) {
+        song.pause()
+        ctrlIcon.classList.remove('fa-pause')
+        ctrlIcon.classList.add('fa-play')
+    }else{
+        song.play()
+        ctrlIcon.classList.remove('fa-play')
+        ctrlIcon.classList.add('fa-pause')
+    }
+})
+
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime
+    }, 500)
+}
